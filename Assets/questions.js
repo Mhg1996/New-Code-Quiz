4d37c52e9432884db514708f59e60ae374b492cb
Mhg1996 <77242548+Mhg1996@users.noreply.github.com>
2021-03-20 11:04:09 -0400

    added third question

--- Assets/questions.js
+++ Assets/questions.js
@@ -7,6 +7,10 @@ const Questions = [
         
             title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
         choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-        answer: "console log"
+        answer: "console log",
+
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
     },
 ];
