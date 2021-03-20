50f44f3a86339bf7283b53cf4c3b2cc6a3373d58
Mhg1996 <77242548+Mhg1996@users.noreply.github.com>
2021-03-20 11:34:13 -0400

    added functions for correct and wrong answers as well as a time penalty

--- Assets/questions.js
+++ Assets/questions.js
@@ -59,18 +59,39 @@ function render(questionIndex) {
     questionsDiv.innerHTML = "";
     ulCreate.innerHTML = "";
     
-    for (var i = 0; i < questions.length; i++) {
+    for (const i = 0; i < questions.length; i++) {
         
-        var userQuestion = questions[questionIndex].title;
-        var userChoices = questions[questionIndex].choices;
+        const userQuestion = questions[questionIndex].title;
+        const userChoices = questions[questionIndex].choices;
         questionsDiv.textContent = userQuestion;
     }
     
     userChoices.forEach(function (newItem) {
-        var listItem = document.createElement("li");
+        const listItem = document.createElement("li");
         listItem.textContent = newItem;
         questionsDiv.appendChild(ulCreate);
         ulCreate.appendChild(listItem);
         listItem.addEventListener("click", (compare));
     })
+}
+
+function compare(event) {
+    var element = event.target;
+
+    if (element.matches("li")) {
+
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+        
+        if (element.textContent == questions[questionIndex].answer) {
+            score++;
+            createDiv.textContent = "Correct! The answer is:  " + questions[questionIndex].answer;
+            
+        } else {
+            
+            secondsLeft = secondsLeft - penalty;
+            createDiv.textContent = "Wrong! The correct answer is:  " + questions[questionIndex].answer;
+        }
+
+    }
 }
